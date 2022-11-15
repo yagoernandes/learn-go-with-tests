c76fbf777e7c1af66d92be8facfa14a31786bf23
Yago Ernandes <yago700@gmail.com>
2022-11-15 18:05:35 -0300

    feat:sumAll with slices

--- 4-arrays-slices/sum_test.go
+++ 4-arrays-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -15,4 +18,39 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+		want := []int{6, 15}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%v', want '%v'", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%v', want '%v'", got, want)
+		}
+	}
+
+	t.Run("sum all elements except the first one", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4, 5})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{})
+		want := []int{0, 0}
+
+		checkSums(t, got, want)
+	})
 }
