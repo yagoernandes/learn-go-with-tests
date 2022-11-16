01fd827b221a06b048f8f5aa9e803bd947ad14ad
Yago Ernandes <yago700@gmail.com>
2022-11-15 22:23:08 -0300

    feat: shapes area * Table driven tests * t.Run for run specific tests (go test -run TestArea/Rectangle) * Declaring structs * Declaring interfaces (parametric polymorphism) * methods to structs * clearer tests

---
+++ 5-structs-methods-interfaces/shapes.go
@@ -0,0 +1,37 @@
+package shapes
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
