6e4aef8be012280b1a043a4483aafa63ea446eb3
Yago Ernandes <yago700@gmail.com>
2022-11-17 09:16:10 -0300

    feat:inject webserver dependecy * dependency injection * io.Writer

---
+++ 8-dependency-injection/di.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %v", name)
+}
+
+// func main() {
+// 	Greet(os.Stdout, "Yago Ernandes\n")
+// }
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
