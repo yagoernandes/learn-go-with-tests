425812afc309b478f53f3bfa79a3d8e73ae893a2
Yago <yago700@gmail.com>
2022-11-30 20:45:32 -0300

    feat:countdown functionality *mocking tests

---
+++ 9-mocking/countdown.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	fmt.Fprintln(out, i)
+	// }
+
+	// fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	// sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
